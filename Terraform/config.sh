83f965add383819e95f92d05d370254c591c2116
JanbaskDevops <148902929+JanbaskDevops@users.noreply.github.com>
2024-07-07 13:02:51 +0530

    Update config.sh

--- Terraform/config.sh
+++ Terraform/config.sh
@@ -1,16 +1,28 @@
 terraform init
 
+
+terraform workspace new dev
+terraform workspace new test
+terraform workspace new qa
+terraform workspace new preprod
+terraform workspace new prod
+
+terraform workspace select dev
 terraform plan -var-file="dev.tfvars"
 terraform apply -var-file="dev.tfvars" -auto-approve
 
+terraform workspace select test
 terraform plan -var-file="test.tfvars"
 terraform apply -var-file="test.tfvars" -auto-approve
 
+terraform workspace select qa
 terraform plan -var-file="qa.tfvars"
 terraform apply -var-file="qa.tfvars" -auto-approve
 
+terraform workspace select preprod
 terraform plan -var-file="preprod.tfvars"
 terraform apply -var-file="preprod.tfvars" -auto-approve
 
+terraform workspace select prod
 terraform plan -var-file="prod.tfvars"
 terraform apply -var-file="prod.tfvars" -auto-approve
