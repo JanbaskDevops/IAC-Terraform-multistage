7683c72bae2c1491ea9bc3cd3148bbe3a463ded1
JanbaskDevops <148902929+JanbaskDevops@users.noreply.github.com>
2024-07-07 12:57:14 +0530

    Update config.sh

--- Terraform/config.sh
+++ Terraform/config.sh
@@ -1,16 +1,16 @@
 terraform init
 
 terraform plan -var-file="dev.tfvars"
-terraform apply -var-file="dev.tfvars"
+terraform apply -var-file="dev.tfvars" -auto-approve
 
 terraform plan -var-file="test.tfvars"
-terraform apply -var-file="test.tfvars"
+terraform apply -var-file="test.tfvars" -auto-approve
 
 terraform plan -var-file="qa.tfvars"
-terraform apply -var-file="qa.tfvars"
+terraform apply -var-file="qa.tfvars" -auto-approve
 
 terraform plan -var-file="preprod.tfvars"
-terraform apply -var-file="preprod.tfvars"
+terraform apply -var-file="preprod.tfvars" -auto-approve
 
 terraform plan -var-file="prod.tfvars"
-terraform apply -var-file="prod.tfvars"
+terraform apply -var-file="prod.tfvars" -auto-approve
